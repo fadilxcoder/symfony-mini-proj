acab12dd5171e9ee40815b042f12c6c503a3ac97
Fadil <rosunmungurudm@gmail.com>
2020-03-14 23:15:51 +0400

    PricingBlock - entity/migration/DB

---
+++ src/Repository/PricingBlockRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\PricingBlock;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method PricingBlock|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PricingBlock|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PricingBlock[]    findAll()
+ * @method PricingBlock[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PricingBlockRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PricingBlock::class);
+    }
+
+    // /**
+    //  * @return PricingBlock[] Returns an array of PricingBlock objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?PricingBlock
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
