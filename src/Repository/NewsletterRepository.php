a4776d84a7645a695db2940a3d40a1f6dee404c5
Fadil Rosun-Mungur <frosun@bocasay.com>
2020-11-29 21:00:28 +0400

    dockerize environment

--- src/Repository/NewsletterRepository.php
+++ src/Repository/NewsletterRepository.php
@@ -4,7 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Newsletter;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+//use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Newsletter|null find($id, $lockMode = null, $lockVersion = null)
--- src/Repository/PricingBlockRepository.php
+++ src/Repository/PricingBlockRepository.php
@@ -4,7 +4,8 @@ namespace App\Repository;
 
 use App\Entity\PricingBlock;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+//use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method PricingBlock|null find($id, $lockMode = null, $lockVersion = null)
