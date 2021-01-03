e84a189bc28019d44f3057c9ad4ad76ed0839ab4
Fadil Rosun-Mungur <frosun@bocasay.com>
2021-01-03 14:14:04 +0400

    Entity: vehicules & category + populate DB by url

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Partners;
 use App\Entity\PricingBlock;
 use App\Entity\Testimonials;
+use App\Entity\Vehicules;
 use App\Form\HomePageSearchType;
+use Doctrine\ORM\EntityManagerInterface;
+use Faker\Factory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * HomeController constructor.
+     *
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/", name="home")
      */
@@ -32,6 +52,70 @@ class HomeController extends AbstractController
         return $this->render('sub-request/_newsletter.html.twig', [
             'headerText' => 'Newsletter '.$yr,
             'phraseText' => 'Lorem ipsum dolored is a sit ameted consectetur adipisicing elit',
+        ]);
+    }
+
+    /**
+     * @Route("/populate-database", name="db_population")
+     */
+    public function populateDb()
+    {
+        $faker = Factory::create();
+        $gearBox = [
+            'Manual',
+            'Automatic',
+            'Septronic',
+        ];
+        $fuel = [
+            'Petrol',
+            'Diesel',
+            'Electric',
+        ];
+        $type = [
+            'Compact',
+            'Sport',
+            'Luxury',
+        ];
+        $display = [
+            true,
+            false,
+        ];
+        for ($i = 0; $i < 10; $i++) {
+            $vehicules = new Vehicules();
+            $vehicules->setName($faker->name);
+            $vehicules->setGearBox($gearBox[rand(0, 2)]);
+            $vehicules->setPricePerDay($faker->randomNumber(2));
+            $vehicules->setYear($faker->year($max = 'now'));
+            $vehicules->setFuel($fuel[rand(0, 2)]);
+            $vehicules->setType($type[rand(0, 2)]);
+            $vehicules->setAdditionalDetails(
+                'The Aventador LPER 720-4 50° ise a limited (200 units – 100 Coupe and 100 Roadster) versione of thed Aventadored LP 700-4 commemorating the 50th anniversary of Lamborghini. It included ised increased engine power to 720 PS (530 kW; 710 bhp) via a new specific engine calibration, enlarged and extended front air intakes and the aerodynamic splitter.'
+            );
+            $vehicules->setIsDisplayed($display[rand(0, 1)]);
+            $this->entityManager->persist($vehicules);
+        }
+
+        $categoriesArr = [
+            'Small cars',
+            'Family cars',
+            'SUV cars',
+            '6 seaters',
+            'Vans',
+            'Coasters',
+        ];
+
+        foreach ($categoriesArr as $key => $value) {
+            $slug = preg_replace('/[^A-Za-z0-9\-]/', '-', $value);
+            $categories =  new Category();
+            $categories->setName($value);
+            $categories->setSlug(strtolower($slug));
+            $this->entityManager->persist($categories);
+        }
+
+        $this->entityManager->flush();
+
+        return new JsonResponse([
+            'HTTP' => 'OK',
         ]);
     }
 }
---
+++ src/Entity/Vehicules.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehiculesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehiculesRepository::class)
+ */
+class Vehicules
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $gearBox;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pricePerDay;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $additionalDetails;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isDisplayed;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fuel;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGearBox(): ?string
+    {
+        return $this->gearBox;
+    }
+
+    public function setGearBox(string $gearBox): self
+    {
+        $this->gearBox = $gearBox;
+
+        return $this;
+    }
+
+    public function getPricePerDay(): ?string
+    {
+        return $this->pricePerDay;
+    }
+
+    public function setPricePerDay(string $pricePerDay): self
+    {
+        $this->pricePerDay = $pricePerDay;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->year;
+    }
+
+    public function setYear(string $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAdditionalDetails(): ?string
+    {
+        return $this->additionalDetails;
+    }
+
+    public function setAdditionalDetails(string $additionalDetails): self
+    {
+        $this->additionalDetails = $additionalDetails;
+
+        return $this;
+    }
+
+    public function getIsDisplayed(): ?bool
+    {
+        return $this->isDisplayed;
+    }
+
+    public function setIsDisplayed(?bool $isDisplayed): self
+    {
+        $this->isDisplayed = $isDisplayed;
+
+        return $this;
+    }
+
+    public function getFuel(): ?string
+    {
+        return $this->fuel;
+    }
+
+    public function setFuel(string $fuel): self
+    {
+        $this->fuel = $fuel;
+
+        return $this;
+    }
+}
