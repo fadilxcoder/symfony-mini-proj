b9fd6b22601f586f09b1500ae5ec94488126f83b
Fadil <rosunmungurudm@gmail.com>
2021-02-14 14:35:08 +0400

    display vehicule page init & DI slug to vehicle obj

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehicules;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehicleController extends AbstractController
+{
+    /**
+     * @Route("/vehicle/{slug}", name="vehicle")
+     */
+    public function index(Vehicules $vehicule): Response
+    {
+        try {
+            dump($vehicule);die;
+            return $this->render('vehicle/index.html.twig', [
+                'controller_name' => 'VehicleController',
+            ]);
+        } catch(\Exception $e) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+    }
+}
